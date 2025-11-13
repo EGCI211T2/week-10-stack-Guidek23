e180c393f44bad59e284dd15346fba24e67fbe62
Guide - uni <jirajade.kor@student.mahidol.edu>
2025-11-13 04:51:43 +0000

    not done

--- stack.h
+++ stack.h
@@ -8,15 +8,22 @@ class Stack {
 public:
     void push(int);
     int pop();
-    Stack();
+    Stack()
+    {
+        top = NULL;
+        size = 0;
+    }
     ~Stack();
 };
 
 
 void Stack::push(int x){
-  NodePtr new_node=new NODE(x);
-  if(new_node){
-            // Left missing for exercises…
+  NodePtr new_node=new NODE(x); //make new node
+  if(new_node)
+  {
+        new_node->get_next() = top;    // Link the new node to the current top
+        top = new_node; // Update the top pointer to the new node
+        size++;
    }
  
          // Left missing for exercises…
@@ -24,19 +31,22 @@ void Stack::push(int x){
 }
 
 int Stack::pop(){
+    if (top == NULL)
+    {
+        cout << "The stack is empty.";
+    }
+    else{
         NodePtr t=top;
         int value;
         value=t->get_value();
-    // Left missing part for exercises
+        get_next();
+        size--;
         delete t;
         return value;
 	//be careful of the empty stack!!!
     }
+    }
 
-Stack::Stack(){
-    //initialize stack
-    
-}
 Stack::~Stack(){
     //delete all remaning stack (i.e. pop all) 
     
